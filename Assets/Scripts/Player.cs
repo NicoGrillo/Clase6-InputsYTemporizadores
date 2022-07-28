965bce80da9826f322cdda62c95d85cbb125314e
NicoGrillo <nicolasgrillo1993@gmail.com>
2022-07-27 22:40:48 -0300

    Desafio casi terminado
    Balas con LifeTime, control de speed, y al apretar Space duplican tamaño

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,6 +8,8 @@ public class Player : MonoBehaviour
     public float speed = 3F;
     public Vector3 direction;
     public bool canShoot = true;
+    private Vector3 directionPlayer;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,28 +19,43 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        /* MODO 1
+        directionPlayer = Vector3.zero;
         if (Input.GetKey(KeyCode.W))
         {
-            Movement(Vector3.forward);
+            directionPlayer += Vector3.forward;
         }
         if (Input.GetKey(KeyCode.S))
         {
-            Movement(Vector3.back);
+            directionPlayer += Vector3.back;
         }
         if (Input.GetKey(KeyCode.A))
         {
-            Movement(Vector3.left);
+            directionPlayer += Vector3.left;
         }
         if (Input.GetKey(KeyCode.D))
         {
-            Movement(Vector3.right);
+            directionPlayer += Vector3.right;
         }
         if (Input.GetKeyDown(KeyCode.Mouse0))
         {
             Shoot();
         }
+        if (directionPlayer != Vector3.zero)
+        {
+            Movement(directionPlayer);
+        }
+        */
+        MoveAxis();
     }
-    private void Movement(Vector3 value)
+    private void MoveAxis()
+    {
+        float ejeHorizontal = Input.GetAxis("Horizontal");
+        float ejeVertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(ejeHorizontal, 0, ejeHorizontal) * speed * Time.deltaTime);
+    }
+
+    private void Movement(Vector3 value) //MODO 1
     {
         transform.Translate(value * speed * Time.deltaTime);
     }
